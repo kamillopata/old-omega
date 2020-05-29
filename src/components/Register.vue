97a92b7b730721eb378c7a61931360298790d122
Kamil Łopata <kamillopata@gmail.com>
2020-05-29 23:07:24 +0200

    add registration with firebase

---
+++ src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>Registration</h1>
+    <input
+      v-model.trim="email"
+      type="email"
+      placeholder="email"
+    ><br>
+    <input
+      v-model.trim="password"
+      type="password"
+      placeholder="password"
+    ><br>
+    <button @click="login">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  name: 'Register',
+
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    async login() {
+      console.log('login');
+      const response = await firebase.auth()
+        .createUserWithEmailAndPassword(this.email, this.password);
+      console.log('query response');
+      console.log(response);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.register {
+  h1 {
+    background: red;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import * as PARAMS from '../params';
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: `${PARAMS.YOUR_API_KEY}`,
+  authDomain: `${PARAMS.YOUR_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${PARAMS.YOUR_PROJECT_ID}.firebaseio.com`,
+  projectId: `${PARAMS.YOUR_PROJECT_ID}`,
+  storageBucket: `${PARAMS.YOUR_PROJECT_ID}.appspot.com`,
+  messagingSenderId: `${PARAMS.YOUR_MESSAGING_SEND_ID}`,
+});
 
 new Vue({
   router,
