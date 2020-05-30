acbbb6c56c015c8cc69f252441140c58cb49480b
Kamil Łopata <kamillopata@gmail.com>
2020-05-30 02:10:55 +0200

    extract vuex user module to separate file

--- src/store/index.js
+++ src/store/index.js
@@ -1,55 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import firebase from 'firebase';
+import user from './user';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-    user: {
-      namespaced: true,
-      state: {
-        id: null,
-        email: null,
-      },
-      getters: {
-        id(state) {
-          return state.id;
-        },
-        email(state) {
-          return state.email;
-        },
-      },
-      actions: {
-        async register({ commit }, event) {
-          const response = await firebase.auth()
-            .createUserWithEmailAndPassword(event.email, event.password);
-          commit('id', response.user.uid);
-          commit('email', response.user.email);
-        },
-        async login({ commit }, event) {
-          const response = await firebase.auth()
-            .signInWithEmailAndPassword(event.email, event.password);
-          console.log(response.user);
-          commit('id', response.user.uid);
-          commit('email', response.user.email);
-        },
-      },
-      mutations: {
-        id(state, id) {
-          state.id = id;
-        },
-        email(state, email) {
-          state.email = email;
-        },
-      },
-    },
+    user,
   },
 });
---
+++ src/store/user.js
@@ -0,0 +1,40 @@
+import firebase from 'firebase';
+
+export default {
+  namespaced: true,
+  state: {
+    id: null,
+    email: null,
+  },
+  getters: {
+    id(state) {
+      return state.id;
+    },
+    email(state) {
+      return state.email;
+    },
+  },
+  actions: {
+    async register({ commit }, event) {
+      const response = await firebase.auth()
+        .createUserWithEmailAndPassword(event.email, event.password);
+      commit('id', response.user.uid);
+      commit('email', response.user.email);
+    },
+    async login({ commit }, event) {
+      const response = await firebase.auth()
+        .signInWithEmailAndPassword(event.email, event.password);
+      console.log(response.user);
+      commit('id', response.user.uid);
+      commit('email', response.user.email);
+    },
+  },
+  mutations: {
+    id(state, id) {
+      state.id = id;
+    },
+    email(state, email) {
+      state.email = email;
+    },
+  },
+};
